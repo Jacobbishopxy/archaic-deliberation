632c271dbabdddcb77ec7d5c6e6f561ec97fc55c
Jacob Xie <jacobbishopxy@gmail.com>
2022-09-21 12:30:38 +0800

    feat: `Castigate` a common db connector

--- castigate/src/helper/connector.rs
+++ castigate/src/helper/connector.rs
@@ -4,8 +4,7 @@ use std::future::Future;
 use std::str::FromStr;
 
 use anyhow::{anyhow, Result};
-use futures::stream::BoxStream;
-use futures::{future::BoxFuture, TryStreamExt};
+use futures::future::BoxFuture;
 use sqlx::mssql::{MssqlPool, MssqlPoolOptions, MssqlRow};
 use sqlx::mysql::{MySqlPool, MySqlPoolOptions, MySqlRow};
 use sqlx::postgres::{PgPool, PgPoolOptions, PgRow};
@@ -36,6 +35,9 @@ pub struct Castigate<T: SqlMeta> {
     pool_options: Option<T>,
 }
 
+const CONN_E_ERR: &str = "Castigate has already been connected!";
+const CONN_N_ERR: &str = "Castigate has not establish a connection yet!";
+
 impl<T: SqlMeta> Castigate<T> {
     pub fn new<S: Into<String>>(conn_str: S) -> Self {
         Self {
@@ -51,127 +53,227 @@ impl<T: SqlMeta> Castigate<T> {
                 self.pool_options = Some(p);
                 Ok(())
             }
-            Some(_) => Err(anyhow!("Castigate has already been connected!")),
+            Some(_) => Err(anyhow!(CONN_E_ERR)),
         }
     }
 
     pub async fn disconnect(&mut self) -> Result<()> {
         match self.pool_options.take() {
-            None => Err(anyhow!("Castigate has not establish a connection yet!")),
+            None => Err(anyhow!(CONN_N_ERR)),
             Some(p) => p.close().await,
+        }
+    }
+
+    pub async fn query<'a, D: Send + Unpin + 'a>(
+        &'a self,
+        sql: &'a str,
+        pipe: fn(T::RowType) -> Result<D>,
+    ) -> Result<Vec<D>> {
+        match self.pool_options.as_ref() {
+            Some(p) => p.query(sql, pipe).await,
+            None => Err(anyhow!(CONN_N_ERR)),
         }
     }
 }
 
 pub trait SqlMeta: Sized {
-    type FutS<'a>: Future<Output = Result<Self>>
+    type FutSelf<'a>: Future<Output = Result<Self>>
     where
         Self: 'a;
 
-    type Fut<'a>: Future<Output = Result<()>>
+    type FutNil<'a>: Future<Output = Result<()>>
     where
         Self: 'a;
 
-    fn new(conn_str: &str) -> Self::FutS<'_>;
+    type RowType;
 
-    fn close(&self) -> Self::Fut<'_>;
+    fn new(conn_str: &str) -> Self::FutSelf<'_>;
 
-    fn is_connected(&self) -> bool;
+    fn close(&self) -> Self::FutNil<'_>;
+
+    fn is_closed(&self) -> bool;
+
+    fn query<'a, T: Send + Unpin + 'a>(
+        &'a self,
+        sql: &'a str,
+        pipe: fn(Self::RowType) -> Result<T>,
+    ) -> BoxFuture<'a, Result<Vec<T>>>;
 }
 
 impl SqlMeta for MssqlPool {
-    type FutS<'a> = impl Future<Output = Result<Self>>;
+    type FutSelf<'a> = impl Future<Output = Result<Self>>;
 
-    type Fut<'a> = impl Future<Output = Result<()>>;
+    type FutNil<'a> = impl Future<Output = Result<()>>;
 
-    fn new(conn_str: &str) -> Self::FutS<'_> {
+    type RowType = MssqlRow;
+
+    fn new(conn_str: &str) -> Self::FutSelf<'_> {
         async move {
             let pg = MssqlPoolOptions::new().connect(conn_str).await?;
             Ok(pg)
         }
     }
 
-    fn close(&self) -> Self::Fut<'_> {
+    fn close(&self) -> Self::FutNil<'_> {
         async move {
             MssqlPool::close(self).await;
             Ok(())
         }
     }
 
-    fn is_connected(&self) -> bool {
-        !self.is_closed()
+    fn is_closed(&self) -> bool {
+        MssqlPool::is_closed(self)
+    }
+
+    fn query<'a, T: Send + Unpin + 'a>(
+        &'a self,
+        sql: &'a str,
+        pipe: fn(Self::RowType) -> Result<T>,
+    ) -> BoxFuture<'a, Result<Vec<T>>> {
+        let q = async move {
+            sqlx::query(sql)
+                .try_map(|r| Ok(pipe(r).map_err(|e| anyhow!(e))))
+                .fetch_all(self)
+                .await
+                .map_err(|e| anyhow!(e))
+                .and_then(|r| r.into_iter().collect::<Result<Vec<T>>>())
+        };
+        Box::pin(q)
     }
 }
 
 impl SqlMeta for MySqlPool {
-    type FutS<'a> = impl Future<Output = Result<Self>>;
+    type FutSelf<'a> = impl Future<Output = Result<Self>>;
 
-    type Fut<'a> = impl Future<Output = Result<()>>;
+    type FutNil<'a> = impl Future<Output = Result<()>>;
 
-    fn new(conn_str: &str) -> Self::FutS<'_> {
+    type RowType = MySqlRow;
+
+    fn new(conn_str: &str) -> Self::FutSelf<'_> {
         async move {
             let pg = MySqlPoolOptions::new().connect(conn_str).await?;
             Ok(pg)
         }
     }
 
-    fn close(&self) -> Self::Fut<'_> {
+    fn close(&self) -> Self::FutNil<'_> {
         async move {
             MySqlPool::close(self).await;
             Ok(())
         }
     }
 
-    fn is_connected(&self) -> bool {
-        !self.is_closed()
+    fn is_closed(&self) -> bool {
+        MySqlPool::is_closed(self)
+    }
+
+    fn query<'a, T: Send + Unpin + 'a>(
+        &'a self,
+        sql: &'a str,
+        pipe: fn(Self::RowType) -> Result<T>,
+    ) -> BoxFuture<'a, Result<Vec<T>>> {
+        let q = async move {
+            sqlx::query(sql)
+                .try_map(|r| Ok(pipe(r).map_err(|e| anyhow!(e))))
+                .fetch_all(self)
+                .await
+                .map_err(|e| anyhow!(e))
+                .and_then(|r| r.into_iter().collect::<Result<Vec<T>>>())
+        };
+        Box::pin(q)
     }
 }
 
 impl SqlMeta for PgPool {
-    type FutS<'a> = impl Future<Output = Result<Self>>;
+    type FutSelf<'a> = impl Future<Output = Result<Self>>;
 
-    type Fut<'a> = impl Future<Output = Result<()>>;
+    type FutNil<'a> = impl Future<Output = Result<()>>;
 
-    fn new(conn_str: &str) -> Self::FutS<'_> {
+    type RowType = PgRow;
+
+    fn new(conn_str: &str) -> Self::FutSelf<'_> {
         async move {
             let pg = PgPoolOptions::new().connect(conn_str).await?;
             Ok(pg)
         }
     }
 
-    fn close(&self) -> Self::Fut<'_> {
+    fn close(&self) -> Self::FutNil<'_> {
         async move {
             PgPool::close(self).await;
             Ok(())
         }
     }
 
-    fn is_connected(&self) -> bool {
-        !self.is_closed()
+    fn is_closed(&self) -> bool {
+        PgPool::is_closed(self)
+    }
+
+    fn query<'a, T: Send + Unpin + 'a>(
+        &'a self,
+        sql: &'a str,
+        pipe: fn(Self::RowType) -> Result<T>,
+    ) -> BoxFuture<'a, Result<Vec<T>>> {
+        let q = async move {
+            sqlx::query(sql)
+                .try_map(|r| Ok(pipe(r).map_err(|e| anyhow!(e))))
+                .fetch_all(self)
+                .await
+                .map_err(|e| anyhow!(e))
+                .and_then(|r| r.into_iter().collect::<Result<Vec<T>>>())
+        };
+        Box::pin(q)
     }
 }
 
-#[tokio::test]
-async fn connection_success() {
-    let url = "postgres://postgres:password@localhost/test";
+#[cfg(test)]
+mod test_connector {
+    use sqlx::Row;
 
-    let pg = PgPoolOptions::new();
-    let mut pool = pg.connect(url).await.expect("Connection success!");
+    use super::*;
 
-    let mut stream = sqlx::query("SELECT * FROM users")
-        .map(|row: PgRow| {
-            // TODO
-        })
-        .fetch(&mut pool);
-
-    while let Some(row) = stream.try_next().await.expect("Next success") {
-        // TODO
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    struct User {
+        email: String,
+        nickname: String,
+        hash: String,
+        role: String,
     }
 
-    let res = sqlx::query("SELECT * FROM users")
-        .map(|row: PgRow| {
-            // TODO
-        })
-        .fetch_all(&mut pool)
-        .await;
+    impl User {
+        fn new(email: String, nickname: String, hash: String, role: String) -> Self {
+            User {
+                email,
+                nickname,
+                hash,
+                role,
+            }
+        }
+
+        fn from_pg_row(row: PgRow) -> Result<Self> {
+            let email: String = row.try_get(0)?;
+            let nickname: String = row.try_get(1)?;
+            let hash: String = row.try_get(2)?;
+            let role: String = row.try_get(3)?;
+
+            Ok(Self::new(email, nickname, hash, role))
+        }
+    }
+
+    #[tokio::test]
+    async fn connection_success() {
+        let url = "postgres://root:secret@localhost:5432/dev";
+
+        let mut ct = Castigate::<PgPool>::new(url);
+        ct.connect().await.expect("Connection success");
+
+        let sql = "SELECT * FROM users";
+
+        let res = ct.query(sql, User::from_pg_row).await;
+
+        println!("{:?}", res);
+
+        assert!(res.is_ok());
+    }
 }
